fdaff9ec582f494c4b16abef48721ade4148cafc
Sebastian Aburto <seba.aburto@gmail.com>
2016-08-23 16:05:45 -0300

    added FibonacciHuge

---
+++ toolbox/src/main/java/FibonacciHuge.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FibonacciHuge {
+
+	static long getFibHuge(long n, long m) {
+		long[] pisanoPeriod = getPisanoPeriod(m);
+		long reminder = n % pisanoPeriod.length;
+		return pisanoPeriod[(int) reminder];
+	}
+
+	static long[] getPisanoPeriod(long m) {
+		if (m == 1) {
+			return new long[] { 0 };
+		}
+		return calc_fib(100000000, m);
+	}
+
+	static long[] calc_fib(final int n, long m) {
+		long[] fib = new long[n + 4];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fib[i] = (fib[i - 1] + fib[i - 2]) % m;
+			if (i > 3 && fib[i - 1] == 0 && fib[i] == 1) {
+				return Arrays.copyOf(fib, i - 1);
+			}
+		}
+		return new long[1];
+	}
+
+	static long getFibonacciHugeNaive(long n, long m) {
+		if (n <= 1)
+			return n;
+
+		long previous = 0;
+		long current = 1;
+
+		for (long i = 0; i < n - 1; ++i) {
+			long tmp_previous = previous;
+			previous = current;
+			current = tmp_previous + current;
+		}
+
+		return current % m;
+	}
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			long n = scanner.nextLong();
+			long m = scanner.nextLong();
+			System.out.println(getFibHuge(n, m));
+		}
+	}
+}
---
+++ toolbox/src/test/java/FibonacciHugeTest.java
@@ -0,0 +1,60 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class FibonacciHugeTest {
+
+	@Test
+	public void testGetPisanoPeriod_1() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(1).length;
+		assertThat(period).isEqualTo(1);
+	}
+
+	@Test
+	public void testGetPisanoPeriod_2() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(2).length;
+		assertThat(period).isEqualTo(3);
+	}
+
+	@Test
+	public void testGetPisanoPeriod_3() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(3).length;
+		assertThat(period).isEqualTo(8);
+	}
+
+	@Test
+	public void testGetPisanoPeriod_239() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(239).length;
+		assertThat(period).isEqualTo(238);
+	}
+
+	@Test
+	public void testGetPisanoPeriod_1000() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(1000).length;
+		assertThat(period).isEqualTo(1500);
+	}
+
+	@Test
+	public void testGetPisanoPeriod_30524() throws Exception {
+		long period = FibonacciHuge.getPisanoPeriod(30524).length;
+		assertThat(period).isEqualTo(1176);
+	}
+
+	@Test
+	public void testGetFibHuge() throws Exception {
+		long fibHuge = FibonacciHuge.getFibHuge(1, 239);
+		assertThat(fibHuge).isEqualTo(1);
+	}
+
+	@Test
+	public void testGetFibHuge_1000() throws Exception {
+		long fibHuge = FibonacciHuge.getFibHuge(239, 1000);
+		assertThat(fibHuge).isEqualTo(161);
+	}
+
+	@Test
+	public void testGetFibHuge_30524() throws Exception {
+		long fibHuge = FibonacciHuge.getFibHuge(2816213588L, 30524);
+		assertThat(fibHuge).isEqualTo(10249);
+	}
+}
