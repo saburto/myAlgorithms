31773aeea589edd3ff518ec9d41d6c8cfad48cdb
Sebastian Aburto <seba.aburto@gmail.com>
2016-08-24 15:47:27 -0300

    fixed the FibonacciPartialSum

--- toolbox/src/test/java/FibonacciPartialSumTest.java
+++ toolbox/src/test/java/FibonacciPartialSumTest.java
@@ -4,22 +4,28 @@ import org.junit.Test;
 
 public class FibonacciPartialSumTest {
 
-	@Test
-	public void testGetFibonacciPartialSum() throws Exception {
-		int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(10, 10);
-		assertThat(fibonacciPartialSum).isEqualTo(5);
-	}
+    @Test
+    public void testGetFibonacciPartialSum() throws Exception {
+        int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(10, 10);
+        assertThat(fibonacciPartialSum).isEqualTo(5);
+    }
 
-	@Test
-	public void testGetFibonacciPartialSum3_7() throws Exception {
-		int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(3, 7);
-		assertThat(fibonacciPartialSum).isEqualTo(1);
-	}
+    @Test
+    public void testGetFibonacciPartialSum3_7() throws Exception {
+        int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(3, 7);
+        assertThat(fibonacciPartialSum).isEqualTo(1);
+    }
 
-	@Test
-	public void testGetFibonacciPartialSum10_200() throws Exception {
-		int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(10, 200);
-		assertThat(fibonacciPartialSum).isEqualTo(2);
-	}
+    @Test
+    public void testGetFibonacciPartialSum10_200() throws Exception {
+        int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(10, 200);
+        assertThat(fibonacciPartialSum).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetFibonacciPartialSum1_100000000() throws Exception {
+        int fibonacciPartialSum = FibonacciPartialSum.getFibonacciPartialSum(1, 100000000);
+        assertThat(fibonacciPartialSum).isEqualTo(5);
+    }
 
 }
